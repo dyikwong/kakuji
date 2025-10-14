df037989777593bf39c1e75e0c55bf1191d4e0aa
dyikwong <56622549+dyikwong@users.noreply.github.com>
2025-10-13 19:34:58 -0700

    make canvas variable to take action on

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,9 +1,11 @@
 
 function Canvas() {
+	let writingCanvas = <canvas id="writing-canvas"></canvas>;
+
 	return (
 		<div>
 			<h2>Canvas</h2>
-			<canvas></canvas>
+			{writingCanvas}
 
 		</div>
 	);
