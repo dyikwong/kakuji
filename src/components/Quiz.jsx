6adc0292efb30962141421a02536a0a54be1f32f
dyikwong <56622549+dyikwong@users.noreply.github.com>
2025-11-19 20:35:58 -0800

    refactoring for data to pass from canvas to quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,27 +2,14 @@ import React, { useState } from 'react';
 import Canvas from './Canvas';
 
 function Quiz(props) {
-	const [canvasCleared, setCanvasCleared] = useState(false);
 	const [submittedWriting, setSubmittedWriting] = useState([]);
 	const [characterListIndex, setCharacterListIndex] = useState(0); // track which character/word
-
-	const clearCanvas = () => {
-		setCanvasCleared(!canvasCleared);
-	};
-
-	const submitWriting = (newWriting) => {
-		alert("here");
-		setSubmittedWriting((submittedWriting) => [...submittedWriting,])
-		setCharacterListIndex(characterListIndex + 1);
-	};
 
 	return (
 		<div>
 			<h2>ホク、きた</h2>
 			<h2>north</h2>
-			<Canvas getCanvasWriting={submitWriting} />
-			<button onClick={clearCanvas}> Clear</button>
-			<button onClick={submitWriting}>Submit</button>
+			<Canvas />
 		</div>
 	)
 }
