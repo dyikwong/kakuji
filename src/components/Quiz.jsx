b4097e3f45a1562abf49d2434b060928e518a8d9
dyikwong <56622549+dyikwong@users.noreply.github.com>
2026-01-06 22:52:08 -0800

    test data in dict to be displayed and iterated through

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,8 +6,26 @@ function Quiz(props) {
 	const [submittedWriting, setSubmittedWriting] = useState([]);
 	const [characterListIndex, setCharacterListIndex] = useState(0); // track which character/word
 
+
+	var characterList = {
+		1: {
+			'kanji': '日',
+			'onyomi': 'ニチ, ジツ',
+			'kunyomi': 'ひ, -び, -か',
+			'meaning': 'day, sun, Japan, counter for days'
+		},
+		2: {
+			'kanji': '一',
+			'onyomi': 'イチ',
+			'kunyomi': 'ひと（つ）',
+			'meaning': 'one'
+		},
+	};
+
 	const location = useLocation();
 	const time = location.state.time;
+
+	const index = Math.floor(Math.random() * Object.keys(characterList).length);
 
 	const submitWriting = (newWriting) => {
 		//setIsSubmit(!isSubmit);
@@ -21,9 +39,9 @@ function Quiz(props) {
 			<Link to="/..">
 				<button>Quit</button>
 			</Link>
-			<h1>北</h1>
-			<h2>ホク、きた</h2>
-			<h2>north</h2>
+			<h2>{characterList[index]['onyomi']}</h2>
+			<h2>{characterList[index]['kunyomi']}</h2>
+			<h2>{characterList[index]['meaning']}</h2>
 			<Canvas getWriting={submitWriting} />
 		</div>
 	)
