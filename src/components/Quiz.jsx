ad8c12386e1e4880fd330ae7406660d01118b31f
dyikwong <56622549+dyikwong@users.noreply.github.com>
2026-03-11 00:13:41 -0700

    set randomization for characters between lists and within lists

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,34 +3,43 @@ import Canvas from './Canvas';
 import Timer from './Timer';
 import { Link, useLocation } from 'react-router-dom';
 
+import n5Data from '../data/n5-kanji.json';
+import n4Data from '../data/n4-kanji.json';
+import n3Data from '../data/n3-kanji.json';
+import n2Data from '../data/n2-kanji.json';
+import n1Data from '../data/n1-kanji.json';
+
 function Quiz(props) {
 	const checkedList = props.content;
 	const timer = props.time;
 
-	var characterList = {
-		0: {
-			'kanji': '日',
-			'onyomi': 'ニチ, ジツ',
-			'kunyomi': 'ひ, -び, -か',
-			'meaning': 'day, sun, Japan, counter for days'
-		},
-		1: {
-			'kanji': '一',
-			'onyomi': 'イチ',
-			'kunyomi': 'ひと（つ）',
-			'meaning': 'one'
-		},
-	};
+	var quizList = [];
+	for (let i = 0; i < checkedList.length; i++) {
+		if (checkedList[i].checked) {
+			if (checkedList[i].title == 'N5') {
+				quizList = quizList.concat(n5Data);
+			} else if (checkedList[i].title == 'N4') {
+				quizList = quizList.concat(n4Data);
+			} else if (checkedList[i].title == 'N3') {
+				quizList = quizList.concat(n3Data);
+			} else if (checkedList[i].title == 'N2') {
+				quizList = quizList.concat(n2Data);
+			} else if (checkedList[i].title == 'N1') {
+				quizList = quizList.concat(n1Data);
+			}
+		}
+	}
+
+	console.log(quizList)
 
 	const [submittedWriting, setSubmittedWriting] = useState([]);
-	const [characterListIndex, setCharacterListIndex] = useState(Math.floor(Math.random() * Object.keys(characterList).length)); // track which character/word
+	const [listIndex, setListIndex] = useState(Math.floor(Math.random() * Object.keys(quizList).length));
+	var characterListIndex = Math.floor(Math.random() * Object.keys(quizList[listIndex]).length);
 
 	const submitWriting = (newWriting) => {
-		//setIsSubmit(!isSubmit);
 		setSubmittedWriting((submittedWriting) => [...submittedWriting, newWriting])
-		setCharacterListIndex(Math.floor(Math.random() * Object.keys(characterList).length));
-		console.log(characterListIndex);
-		console.log(submittedWriting);
+		setListIndex(Math.floor(Math.random() * Object.keys(quizList).length));
+		characterListIndex = Math.floor(Math.random() * Object.keys(quizList[listIndex]).length);
 	};
 
 	return (
@@ -40,9 +49,10 @@ function Quiz(props) {
 			</Link>
 			<Timer time={timer} />
 			<h2>{characterListIndex}</h2>
-			<h2>{characterList[characterListIndex]['onyomi']}</h2>
-			<h2>{characterList[characterListIndex]['kunyomi']}</h2>
-			<h2>{characterList[characterListIndex]['meaning']}</h2>
+			<h2>{listIndex}</h2>
+			<h2>{quizList[listIndex][characterListIndex]['onyomi']}</h2>
+			<h2>{quizList[listIndex][characterListIndex]['kunyomi']}</h2>
+			<h2>{quizList[listIndex][characterListIndex]['meaning']}</h2>
 			<Canvas getWriting={submitWriting} />
 		</div>
 	)
